9378c45900b05ec7ca9333af516814d21982ef88
Olumide Ogundele <olumideralph@gmail.com>
2023-11-04 12:16:10 +0000

    print the first 100 products

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::time::Duration;
 
 use fantoccini::{ClientBuilder, Locator, elements::Element};
+use scraper::{Html, Selector};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -18,7 +19,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             "div.dataTables_wrapper div.dataTables_length select",
         ))
         .await?;
-    
+
     // Display 100 items.
     select_element
         .clone()
@@ -39,23 +40,38 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         ))
         .await?;
 
-    let content = table_body.html(false).await?;
-    println!("{}", content);
-
     let tbody_rows = client
         .find_all(Locator::Css(
             "table.dataTable tbody tr"
         ))
         .await?;
 
+    let td_selector = Selector::parse("td").unwrap();
     for row in tbody_rows.as_slice() {
-        let value = row.html(false).await?;
-        println!("Result: {}", value)
+        let html = row.html(false).await?;
+        let raw_string = format!(r#"<table>{}</table>"#, html);
+        let fragment = Html::parse_fragment(&raw_string);
+
+        println!("{}", "===========================");
+        for element in fragment.select(&td_selector) {
+            println!("{}", element.text().collect::<Vec<_>>().join(","));
+        }
+        // let value = row.html(false).await?;
+        // println!("Result: {}", value)
     }
 
     println!("{}", tbody_rows.len());
 
     client.close().await?;
+
+    // let html = r#"<table><tr role="row" class="odd"><td class="sorting_1 dtr-control">Afrab Oral Rehydration Salt</td><td><a href="/ingredient/products/191">Oral Rehydration Salts</a></td><td><a href="/products/details/2971">A11-100055</a></td><td>Granules</td><td>Oral</td><td>20.5 g</td><td><a href="/applicant/products/82">Afrab-Chem Limited</a></td><td>2021-03-29</td><td>Active</td></tr></table>"#;
+
+    // let fragment = Html::parse_fragment(html);
+    // let selector = Selector::parse("td").unwrap();
+
+    // for element in fragment.select(&selector) {
+    //     println!("{}", element.value().name());
+    // }
 
     Ok(())
 }
